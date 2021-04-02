cfaa9e07673cd8948684d0a952ef8c46926e8d6e
Hiroki.T <hirodora@me.com>
2021-04-03 01:13:22 +0900

    add: 本の本文検索を実装

---
+++ 創造システム理論/main.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+with open('secret.json', 'r') as secretfp:
+    COOKIE = json.load(secretfp)['COOKIE']
+
+
+
+def text_result(book_id: str, query: str):
+    headers = {
+        'cookie': COOKIE
+    }
+    resp = requests.get(
+        f'https://books.google.co.jp/books?id={book_id}&jscmd=SearchWithinVolume&q={query}',
+        headers=headers
+    )
+    return resp.json()
+
+
+book_id = 'wY75DwAAQBAJ'
+query = 'インターネット'
+print(text_result(book_id, query))
