ba4c58f900160bfd46eb31830d6758763cfbf389
Hiroki.T <hirodora@me.com>
2021-10-01 19:52:21 +0900

    create: sketch.js

---
+++ design-programming/1/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    background(31, 127, 255);
+}
+ 
+function draw(){
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(width/2, height/2, height/2);
+}
