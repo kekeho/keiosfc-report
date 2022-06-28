59e14803f169c388beadfae339ca4c7e90779678
Hiroki.T <hirodora@me.com>
2022-06-28 15:05:03 +0900

    add: 10

--- syspro-basic/10/ex.c
+++ syspro-basic/10/ex.c
@@ -1,8 +1,13 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 #define NAMELEN 10
+#define NLISTLEN 4
+
+
 
 struct name {
   char first[NAMELEN];
@@ -11,11 +16,11 @@ struct name {
 
 
 int namecmp(struct name *, struct name *);
-void printnames(struct name *, int nsize);
+void printnames(struct name *, size_t nsize);
 
 
 int main() {
-    struct name nlist[4] = {
+    struct name nlist[NLISTLEN] = {
         {"Taro","Keio"},
         {"Hanako", "Keio"},
         {"Yuichi", "Fukuzawa"},
@@ -23,8 +28,8 @@ int main() {
     };
 
     printf("=== ソート前 ===\n");
-    printnames(nlist, 4);
-    qsort(nlist, 4, sizeof(struct name), (void *)namecmp);
+    printnames(nlist, NLISTLEN);
+    qsort(nlist, NLISTLEN, sizeof(struct name), (void *)namecmp);
     printf("=== ソート後 ===\n");
     printnames(nlist, 4);
 }
@@ -36,21 +41,21 @@ int namecmp(struct name *a, struct name *b) {
 
     // 姓で並べ替え
     for (size_t i = 0; i < NAMELEN; i++) {
-        if (a->last[i] < b->last[i]) return 0;
-        if (a->last[i] > b->last[i]) return 1;
+        if ((unsigned char)a->last[i] < (unsigned char)b->last[i]) return 0;
+        if ((unsigned char)a->last[i] > (unsigned char)b->last[i]) return 1;
     }
 
     // 名で並べ替え
     for (size_t i = 0; i < NAMELEN; i++) {
-        if (a->first[i] < b->first[i]) return 0;
-        if (a->first[i] > b->first[i]) return 1;
+        if ((unsigned char)a->first[i] < (unsigned char)b->first[i]) return 0;
+        if ((unsigned char)a->first[i] > (unsigned char)b->first[i]) return 1;
     }
 
     return 0; // 同じ文字列であれば, とりあえず0
 }
 
 
-void printnames(struct name *namelist, int nsize) {
+void printnames(struct name *namelist, size_t nsize) {
     for (size_t i = 0; i < nsize; i++) {
         printf("%s %s\n", namelist[i].first, namelist[i].last);
     }
